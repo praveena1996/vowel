e883fb730812c61cd75c6a7e411358ac91dcb55b
praveena1996 <praveenaselva20@gmail.com>
2017-07-20 21:25:56 +0530

    Create fead.java

---
+++ fead.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+public class fead
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+char n;
+BufferReader d=new BufferReader(new InputStreamBuffer(System.in));
+n=(char)d.read;
+switch(n)
+{
+case a:
+System.out.print("vowel");
+break;
+case e:
+System.out.print("vowel");
+break;
+case i:
+System.out.print("vowel");
+break;
+case o:
+System.out.print("vowel");
+break;
+case u:
+System.out.print("vowel");
+break;
+case default:
+System.out.print("constant");
+break;
+}
+}
+}
